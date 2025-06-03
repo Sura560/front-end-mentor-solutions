4c60c46c7a2d69bc382a67a49f4f9a557fec31d1
bitanya-gezahegn <bitanyagidey@gmail.com>
2025-06-03 08:12:45 -0700

    fix

--- interactive-rating-component-main/source.js
+++ interactive-rating-component-main/source.js
@@ -5,7 +5,16 @@ const button = document.getElementById("btn")
  button.addEventListener('click', function(){
    console.log(button.value)
  })
+ const button2= document.getElementById("btn2")
+ button2.addEventListener('click', function(){
+   console.log(button2.value)
+ })
    
+let rate;
+function rating(value) {
+rate= value;
+}
+ 
  function clicked(){
    let str = rating();
     console.log(str)
@@ -17,7 +26,7 @@ const button = document.getElementById("btn")
   <div class="card-rate">
      <img src="images/illustration-thank-you.svg" alt="">
         <div class="rate-value">
-           <p>You selected  out of 5</p>
+           <p>You selected ${rate} out of 5</p>
         </div>
       <h1> Thank you!</h1>
       <p>We appreciate you taking the time to give a rating. If you ever need more support, 
