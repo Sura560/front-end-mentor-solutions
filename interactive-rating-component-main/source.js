2c8600194223d646b23c9d245087f3513e063eab
Sura560 <136069095+Sura560@users.noreply.github.com>
2025-06-03 21:59:54 +0300

    a

--- interactive-rating-component-main/source.js
+++ interactive-rating-component-main/source.js
@@ -2,25 +2,13 @@ const card = document.getElementById("card")
 
 const btn = document.getElementsByTagName("button");
 const button = document.getElementById("btn")
- button.addEventListener('click', function(){
-   console.log(button.value)
- })
- const button2= document.getElementById("btn2")
- button2.addEventListener('click', function(){
-   console.log(button2.value)
- })
+ 
    
 let rate;
 function rating(value) {
 rate= value;
 }
  
- function clicked(){
-   let str = rating();
-    console.log(str)
-    console.log('clicked')
- }
-
        function submit(){
      card.innerHTML = `
   <div class="card-rate">
